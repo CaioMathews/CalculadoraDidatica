9e6fa63f0972f0a17c92f1cc10a62da9f7123da9
CaioMathews <cmff@cesar.school>
2025-11-19 15:40:07 -0300

    bcd

--- CalculadoraDidatica_CMFF.asm
+++ CalculadoraDidatica_CMFF.asm
@@ -11,7 +11,9 @@
     str_bin:       .asciiz "\n--- [a] Binario (Base 2) ---\n"
     str_oct:       .asciiz "\n--- [b] Octal (Base 8) ---\n"
     str_hex:       .asciiz "\n--- [c] Hexadecimal (Base 16) ---\n"
+    str_bcd:       .asciiz "\n--- [d] BCD (Binary Coded Decimal) ---\n"
     
+    msg_space:     .asciiz " "
     msg_step:      .asciiz "\nPasso: "
     msg_div:       .asciiz "Div: "
     msg_quo:       .asciiz " | Quo: "
@@ -81,6 +83,13 @@ case_conversions:
     move $a0, $s0
     li $a1, 16     
     jal generic_base_converter
+    
+    # BCD
+    li $v0, 4
+    la $a0, str_bcd
+    syscall
+    move $a0, $s0
+    jal process_bcd
     
     j main_loop
 
@@ -185,6 +194,67 @@ generic_base_converter:
         addi $sp, $sp, 16
         jr $ra
 
+process_bcd:
+    addi $sp, $sp, -12
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+    move $s0, $a0
+    li $s1, 0
+    li $t9, 10
+
+    bcd_loop_div:
+        div $s0, $t9
+        mflo $s0
+        mfhi $t0
+        sub $sp, $sp, 4
+        sw $t0, 0($sp)
+        addi $s1, $s1, 1
+        bgt $s0, 0, bcd_loop_div
+    
+    li $v0, 4
+    la $a0, msg_res_final
+    syscall
+
+    bcd_pop_print:
+        beq $s1, 0, end_bcd
+        lw $a0, 0($sp)
+        addi $sp, $sp, 4
+        addi $s1, $s1, -1
+        
+        li $a1, 4         
+        jal print_binary_n_bits
+        
+        li $v0, 4
+        la $a0, msg_space
+        syscall
+        j bcd_pop_print
+
+    end_bcd:
+        lw $ra, 0($sp)
+        lw $s0, 4($sp)
+        lw $s1, 8($sp)
+        addi $sp, $sp, 12
+        jr $ra
+
+print_binary_n_bits:
+
+    move $t8, $a0
+    move $t9, $a1
+    addi $t9, $t9, -1
+    
+    bit_loop:
+        blt $t9, 0, end_print_bits
+        srlv $a0, $t8, $t9
+        andi $a0, $a0, 1
+        li $v0, 1
+        syscall
+        addi $t9, $t9, -1
+        j bit_loop
+        
+    end_print_bits:
+        jr $ra
+        
 exit_program:
     li $v0, 10
     syscall
